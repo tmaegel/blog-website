804f70859254808b9108d7f2037fb75c4383c299
Toni Mägel <tmaegel@posteo.de>
2023-11-02 14:03:54 +0100

    Update config

--- content/pages/about.md
+++ content/pages/about.md
@@ -1,3 +1,3 @@
-Title: Über
+Title: About
 
 About ...
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,9 +4,11 @@ SITEURL = "https://tmaegel.github.io/blog-website/"
 
 PATH = "content"
 
-TIMEZONE = "Europe/Berlin"
+ARTICLE_ORDER_BY = "date"
+PAGE_ORDER_BY = "reversed-basename"
 
-DEFAULT_LANG = "de"
+TIMEZONE = "Europe/Berlin"
+DEFAULT_LANG = "en"
 
 # Uncomment following line if you want document-relative URLs when developing
 # Can be useful in development, but set to False when you're ready to publish
@@ -23,11 +25,7 @@ AUTHOR_FEED_RSS = None
 LINKS = ()
 
 # Social widget
-SOCIAL = (
-    ("github", "https://github.com/tmaegel/"),
-    ("linkedin", "https://de.linkedin.com/in/toni-maegel"),
-    ("envelope-fill", "mailto:tmaegel@posteo.de"),
-)
+SOCIAL = (("github", "https://github.com/tmaegel/"),)
 
 GITHUB_URL = "https://github.com/tmaegel/"
 
@@ -35,11 +33,10 @@ DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = True
 MENUITEMS = (
     ("Posts", "/index.html"),
-    ("Themen", "/categories.html"),
     ("Tags", "/tags.html"),
 )
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 2
 
 PLUGIN_PATH = ["plugins"]
 PLUGINS = ["better_codeblock_line_numbering_fork"]
